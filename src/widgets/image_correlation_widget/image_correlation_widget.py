3f26f85d5b0f548f95d56497debaaecbaa51577d
yuramorozov01 <yura_morozov01@mail.ru>
2022-02-25 19:13:19 +0300

    refactor: add python project files to src directory (DSP-4)

--- src/widgets/image_correlation_widget/image_correlation_widget.py
+++ src/widgets/image_correlation_widget/image_correlation_widget.py
@@ -26,7 +26,7 @@ class ImageCorrelationWidget(QtWidgets.QWidget):
         self._init_controls()
 
     def _init_controls(self):
-        lb_image_1 = self._widgets_creator.create_label('', width=350, height=450)
+        lb_image_1 = self._widgets_creator.create_label('', width=250, height=350)
         lb_open_image_1, pb_open_image_1, tmp_widget_image_1 = \
             self._widgets_creator.create_label_with_pushbutton(
                 'Image 1',
@@ -40,7 +40,7 @@ class ImageCorrelationWidget(QtWidgets.QWidget):
             layout='v'
         )
 
-        lb_image_2 = self._widgets_creator.create_label('', width=350, height=450)
+        lb_image_2 = self._widgets_creator.create_label('', width=250, height=350)
         lb_open_image_2, pb_open_image_2, tmp_widget_image_2 = \
             self._widgets_creator.create_label_with_pushbutton(
                 'Image 2',
@@ -95,3 +95,28 @@ class ImageCorrelationWidget(QtWidgets.QWidget):
             array_to_load_image = cv2.imread(filename, cv2.IMREAD_COLOR)
             cv2.cvtColor(array_to_load_image, cv2.COLOR_BGR2RGB, array_to_load_image)
             self._lb_image_set_image(label, array_to_load_image)
+
+    def normxcorr2(self, template, image, mode="full"):
+        if np.ndim(template) > np.ndim(image) or \
+                len([i for i in range(np.ndim(template)) if template.shape[i] > image.shape[i]]) > 0:
+            print("normxcorr2: TEMPLATE larger than IMG. Arguments may be swapped.")
+
+        template = template - np.mean(template)
+        image = image - np.mean(image)
+
+        a1 = np.ones(template.shape)
+
+        ar = np.flipud(np.fliplr(template))
+        out = scipy.signal.fftconvolve(image, ar.conj(), mode=mode)
+
+        image = scipy.signal.fftconvolve(np.square(image), a1, mode=mode) - \
+                np.square(scipy.signal.fftconvolve(image, a1, mode=mode)) / (np.prod(template.shape))
+
+        image[np.where(image < 0)] = 0
+
+        template = np.sum(np.square(template))
+        out = out / np.sqrt(image * template)
+
+        out[np.where(np.logical_not(np.isfinite(out)))] = 0
+        
+        return out
